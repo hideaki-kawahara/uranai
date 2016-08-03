64b97cae8faab251ab654e710890961f3ac61758
川原 英明 <inukkoro0204@gmail.com>
2016-08-04 01:32:03 +0900

    dropdown add

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -26,10 +26,13 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	public void OnValueChanged(int result)
 	{
+		Debug.Log (result );
 		if (result == 0) {
 			Debug.Log ("選択してください");
 		} else {
-			Debug.Log ((string)jsonData ["horoscope"] [dd] [result] ["content"]);
+			string aa = GetAtomFromBirthday (3, 9);
+//			Debug.Log (aa);
+//			Debug.Log ((string)jsonData ["horoscope"] [dd] [result] ["content"]);
 
 		}
 	}
@@ -37,6 +40,23 @@ public class NewBehaviourScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	public string GetAtomFromBirthday(int month, int day)
+	{
+		float fBirthDay = Convert.ToSingle(month.ToString("D") + '.' + day.ToString("D2"));
+		float[] atomBound = { 1.20F, 2.20F, 3.21F, 4.21F, 5.21F, 6.22F, 7.23F, 8.23F, 9.23F, 10.23F, 11.21F, 12.22F, 13.20F };
+		string[] atoms = { "水瓶座", "魚座", "牡羊座", "牡牛座", "双子座", "蟹座", "獅子座", "乙女座", "天秤座", "蠍座", "射手座", "山羊座" };
+		string ret = string.Empty;
+		for(int i = 0; i < atomBound.Length - 1; i++)
+		{
+			if (atomBound[i] <= fBirthDay && atomBound[i + 1] > fBirthDay)
+			{
+				ret = atoms[i];
+				break;
+			}
+		}
+		return ret;
 	}
 }
 
