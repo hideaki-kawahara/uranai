a04b6b4aa9e526793fadcb62d6d3ea0557bb2eba
Hideaki_Kawahara <inukkoro0204@gmail.com>
2016-08-04 18:34:34 +0900

    r
    2 dropdown add

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -3,14 +3,17 @@ using System.Collections;
 using System;
 using LitJson;
 using UnityEngine.UI;
+using System.Text.RegularExpressions; 
 
 public class NewBehaviourScript : MonoBehaviour {
 
 	private string path = "http://api.jugemkey.jp/api/horoscope/free";
 	public string dd = "";
 	public JsonData jsonData;
+	public DecodeData[] decodeData;
 
 	public Text text;
+	public string aw;
 
 	IEnumerator Start() {
 		dd = DateTime.Now.ToString("yyyy/MM/dd");
@@ -20,21 +23,32 @@ public class NewBehaviourScript : MonoBehaviour {
 			if (!string.IsNullOrEmpty (www.error)) {
 				yield break;
 			}
+			aw = www.text.Replace (dd, "hoge");
 			jsonData = JsonMapper.ToObject(www.text);
+			decodeData = JsonMapper.ToObject<DecodeData[]>(aw);
 		}
 	}
 
-	public void OnValueChanged(int result)
+	public void OnValueChanged(int res)
 	{
-		Debug.Log (result );
-		if (result == 0) {
-			Debug.Log ("選択してください");
-		} else {
-			string aa = GetAtomFromBirthday (3, 9);
-//			Debug.Log (aa);
+		Dropdown dropM = GameObject.FindGameObjectWithTag("month").GetComponent<Dropdown>();
+		Dropdown dropD = GameObject.FindGameObjectWithTag("day").GetComponent<Dropdown>();
+//		Debug.Log (dropM.value + ":" + dropD.value);
+		if (dropM.value == 0 || dropD.value == 0) {
+			return;
+		}
+		string sss = GetAtomFromBirthday (dropM.value.ToString("00"), dropD.value.ToString("00"));
+		Debug.Log (sss);
+//		Debug.Log (decodeData[0].horoscope.hoge[0].content);
+
+//		foreach(JsonData r in jsonData ["horoscope"] [dd] ) {
+//			string sign = (string)r ["sign"];
+//			if (sign.Equals (sss)) {
+//				Debug.Log ((string)r ["content"]);
+//			}
+//		}
 //			Debug.Log ((string)jsonData ["horoscope"] [dd] [result] ["content"]);
 
-		}
 	}
 
 	// Update is called once per frame
@@ -42,21 +56,68 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	}
 
-	public string GetAtomFromBirthday(int month, int day)
+	public string GetAtomFromBirthday(string month, string day)
 	{
-		float fBirthDay = Convert.ToSingle(month.ToString("D") + '.' + day.ToString("D2"));
-		float[] atomBound = { 1.20F, 2.20F, 3.21F, 4.21F, 5.21F, 6.22F, 7.23F, 8.23F, 9.23F, 10.23F, 11.21F, 12.22F, 13.20F };
-		string[] atoms = { "水瓶座", "魚座", "牡羊座", "牡牛座", "双子座", "蟹座", "獅子座", "乙女座", "天秤座", "蠍座", "射手座", "山羊座" };
-		string ret = string.Empty;
-		for(int i = 0; i < atomBound.Length - 1; i++)
-		{
-			if (atomBound[i] <= fBirthDay && atomBound[i + 1] > fBirthDay)
-			{
-				ret = atoms[i];
-				break;
-			}
+		string md = month + day;
+		if (Regex.IsMatch (md, @"\A0(?:3(?:2[123456789]|3[01])|4(?:0[123456789]|1\d))\z")) {
+			return "牡羊座";
 		}
-		return ret;
+		if (Regex.IsMatch (md, @"\A0(?:5(?:0[123456789]|1\d|20)|4(?:2\d|30))\z")) {
+			return "牡牛座";
+		}
+		if (Regex.IsMatch (md, @"\A0(?:6(?:0[123456789]|2[01]|1\d)|5(?:2[123456789]|3[01]))\z")) {
+			return "双子座";
+		}
+		if (Regex.IsMatch (md, @"\A0(?:7(?:0[123456789]|2[012]|1\d)|6(?:2[23456789]|30))\z")) {
+			return "蟹座";
+		}
+		if (Regex.IsMatch (md, @"\A0(?:8(?:0[123456789]|2[012]|1\d)|7(?:2[3456789]|3[01]))\z")) {
+			return "獅子座";
+		}
+		if (Regex.IsMatch (md, @"\A0(?:9(?:0[123456789]|2[012]|1\d)|8(?:2[3456789]|3[01]))\z")) {
+			return "乙女座";
+		}
+		if (Regex.IsMatch (md, @"\A(?:10(?:0[123456789]|2[0123]|1\d)|09(?:2[3456789]|30))\z")) {
+			return "天秤座";
+		}
+		if (Regex.IsMatch (md, @"\A1(?:1(?:0[123456789]|2[01]|1\d)|0(?:2[456789]|3[01]))\z")) {
+			return "蟹座";
+		}
+		if (Regex.IsMatch (md, @"\A1(?:2(?:0[123456789]|2[01]|1\d)|1(?:2[23456789]|30))\z")) {
+			return "射手座";
+		}
+		if (Regex.IsMatch (md, @"\A(?:12(?:2[23456789]|3[01])|01(?:0[123456789]|1\d))\z")) {
+			return "山羊座";
+		}
+		if (Regex.IsMatch (md, @"\A0(?:2(?:0[123456789]|1[012345678])|1(?:3[01]|2\d))\z")) {
+			return "水瓶座";
+		}
+		if (Regex.IsMatch (md, @"\A0(?:3(?:0[123456789]|1\d|20)|2(?:2\d|19))\z")) {
+			return "魚座";
+		}
+		return "";
+	}
+
+	public class DecodeData
+	{
+		public Horoscope horoscope;
+	}
+	public class Horoscope
+	{
+		public Hoge[] hoge;
+	}
+	public class Hoge
+	{
+		public string content;
+		public string item;
+		public int money;
+		public int total;
+		public int job;
+		public string color;
+		public int day;
+		public int love;
+		public int rank;
+		public string sign;
 	}
 }
 
